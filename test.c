d00b85ed632f0247e99b17813875ccb5db999e21
František Dvořák <valtri@civ.zcu.cz>
2014-05-13 16:21:09 +0200

    Test any path.

--- test.c
+++ test.c
@@ -5,9 +5,13 @@
 #include "tinyafs.h"
 
 
-int main() {
+int main(int argn, char *argc[]) {
 	char *cell;
 	struct Acl acl;
+	const char *path = "/afs/zcu.cz";
+
+	if (argn > 1) path = argc[1];
+	printf("using path: %s\n", path);
 
 	if (!has_afs()) {
 		printf("has AFS: no\n");
@@ -15,13 +19,13 @@ int main() {
 	}
 	printf("has AFS: yes\n");
 
-	if (get_cell("/afs/zcu.cz", &cell) != 0) {
+	if (get_cell(path, &cell) != 0) {
 		cell = strdup("(error)");
 	}
 	printf("cell: %s\n", cell);
 	free(cell);
 
-	if (get_acl("/afs/zcu.cz", &acl) != 0) {
+	if (get_acl(path, &acl) != 0) {
 		perror(NULL);
 	} else {
 		printf("acl: ");
