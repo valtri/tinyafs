924d21514d797bd4ae857157e4860916343d9d21
František Dvořák <valtri@civ.zcu.cz>
2014-05-14 22:24:55 +0200

    "Contextualize", print progress and statistics, transactions.

--- scan.c
+++ scan.c
@@ -1,3 +1,4 @@
+#include <sys/time.h>
 #include <errno.h>
 #include <getopt.h>
 #include <stdio.h>
@@ -23,18 +24,36 @@
 #define EXIT_ERRORS 1
 #define EXIT_FATAL 2
 
+#define COUNT_COMMIT 5000
+#define COUNT_PROGRESS 100000
+
+
+
+typedef struct {
+	char *basedir; /* start directory */
+	char *volume;  /* name of volume */
+
+	size_t basedir_len;
+	glite_lbu_DBContext db;
+	glite_lbu_Statement points_stmt, rights_stmt;
+
+	int was_err;
+	size_t count;
+	struct timeval begin, end;
+} ctx_t;
+
+
 
 char *program_name = NULL;
-
 char *dbcs = NULL;
 
-char *basedir = NULL;
-size_t basedir_len = 0;
-char *volume = NULL;
-glite_lbu_DBContext db;
-glite_lbu_Statement points_stmt, rights_stmt;
-int err = 0;
-
+ctx_t ctx = {
+	basedir: NULL,
+	basedir_len: 0,
+	volume: NULL,
+	was_err: 0,
+	count: 0,
+};
 
 const char *optstring = "hc:";
 
@@ -43,6 +62,7 @@ const struct option longopts[] = {
 	{ "config", required_argument, NULL, 'c' },
 	{ NULL, 0, NULL, 0 }
 };
+
 
 
 static void usage() {
@@ -137,7 +157,7 @@ static void print_DBError(glite_lbu_DBContext db) {
 }
 
 
-static void save_rights(glite_lbu_Statement stmt, const char *volume, const char *relpath, const struct AclEntry *entry, int negative) {
+static void save_rights(ctx_t *ctx, const char *relpath, const struct AclEntry *entry, int negative) {
 	char rights[MAXRIGHTS + 2];
 
 	if (negative) {
@@ -146,69 +166,81 @@ static void save_rights(glite_lbu_Statement stmt, const char *volume, const char
 	}
 	rights2str(entry->rights, rights + negative);
 
-	if (glite_lbu_ExecPreparedStmt(stmt, 4,
-		GLITE_LBU_DB_TYPE_VARCHAR, volume,
+	if (glite_lbu_ExecPreparedStmt(ctx->rights_stmt, 4,
+		GLITE_LBU_DB_TYPE_VARCHAR, ctx->volume,
 		GLITE_LBU_DB_TYPE_VARCHAR, relpath,
 		GLITE_LBU_DB_TYPE_VARCHAR, entry->name,
 		GLITE_LBU_DB_TYPE_VARCHAR, rights
 	    ) != 1) {
-		print_DBError(db);
-		err = 1;
+		print_DBError(ctx->db);
+		ctx->was_err = 1;
 	}
 }
 
 
-static void save_point(glite_lbu_Statement stmt, const char *volume, const char *relpath, const char *mount) {
-	if (glite_lbu_ExecPreparedStmt(stmt, 3,
-		GLITE_LBU_DB_TYPE_VARCHAR, volume,
+static void save_point(ctx_t *ctx, const char *relpath, const char *mount) {
+	if (glite_lbu_ExecPreparedStmt(ctx->points_stmt, 3,
+		GLITE_LBU_DB_TYPE_VARCHAR, ctx->volume,
 		GLITE_LBU_DB_TYPE_VARCHAR, relpath,
 		GLITE_LBU_DB_TYPE_VARCHAR, mount
 	    ) != 1) {
-		print_DBError(db);
-		err = 1;
+		print_DBError(ctx->db);
+		ctx->was_err = 1;
 	}
 }
 
 
-static int action(const char *path, int level, void *data __attribute__((unused))) {
+static int action(const char *path, int level, void *data) {
 	char *mount;
 	const char *relpath;
 	struct Acl acl;
 	size_t i;
+	ctx_t *ctx = (ctx_t *)data;
 
 	if (level >= 200) {
 		fprintf(stderr, "level too high, skipping: %s\n", path);
 		return BROWSE_ACTION_SKIP;
 	}
 
-	if (strncmp(path, basedir, basedir_len) == 0) relpath = path + basedir_len;
+	if (strncmp(path, ctx->basedir, ctx->basedir_len) == 0) relpath = path + ctx->basedir_len;
 	else relpath = path;
 	if (relpath[0] == '/') relpath++;
 
 	if (list_mount(path, &mount) == 0) {
-		printf("%d: %s, mountpoint: %s\n", level, path, mount);
 		if (level != 0) {
-			save_point(points_stmt, volume, relpath, mount);
+			printf("(%s -> %s) ", relpath, mount); fflush(stdout);
+			save_point(ctx, relpath, mount);
 			free(mount);
 			return BROWSE_ACTION_SKIP;
 		} else {
 			free(mount);
 		}
-	} else {
-		/*printf("%d: %s\n", level, path);*/
-		if (level <= 2) printf("%02d: %s:%s\n", level, volume, relpath);
 	}
 
 	if (get_acl(path, &acl) != 0) {
 		fprintf(stderr, "error getting ACLs for '%s': %s\n", path, strerror(errno));
-		err = 1;
+		ctx->was_err = 1;
 		return BROWSE_ACTION_OK;
 	}
-	for (i = 0; i < acl.nplus; i++) save_rights(rights_stmt, volume, relpath, acl.plus + i, 0);
-	for (i = 0; i < acl.nminus; i++) save_rights(rights_stmt, volume, relpath, acl.minus + i, 1);
+	for (i = 0; i < acl.nplus; i++) save_rights(ctx, relpath, acl.plus + i, 0);
+	for (i = 0; i < acl.nminus; i++) save_rights(ctx, relpath, acl.minus + i, 1);
 	free_acl(&acl);
 
+	ctx->count++;
+	if ((ctx->count % COUNT_COMMIT) == 0) {
+		glite_lbu_Commit(ctx->db);
+		glite_lbu_Transaction(ctx->db);
+	}
+	if ((ctx->count % COUNT_PROGRESS) == 0) {
+		printf("%zd ", ctx->count); fflush(stdout);
+	}
+
 	return BROWSE_ACTION_OK;
+}
+
+
+static double timeval2double(const struct timeval *tv) {
+	return tv->tv_sec + ((double)tv->tv_usec) / 1000000;
 }
 
 
@@ -235,9 +267,9 @@ int main(int argc, char *argv[]) {
 		usage();
 		return EXIT_FATAL;
 	}
-	volume = argv[optind++];
-	basedir = argv[optind++];
-	basedir_len = strlen(basedir);
+	ctx.volume = argv[optind++];
+	ctx.basedir = argv[optind++];
+	ctx.basedir_len = strlen(ctx.basedir);
 	if (!dbcs) dbcs = strdup(DEFAULT_DBCS);
 
 	if (!has_afs()) {
@@ -246,29 +278,36 @@ int main(int argc, char *argv[]) {
 	}
 
 	glite_common_log_init();
-	if (glite_lbu_InitDBContext(&db, GLITE_LBU_DB_BACKEND_MYSQL, "DB") != 0) {
+	if (glite_lbu_InitDBContext(&ctx.db, GLITE_LBU_DB_BACKEND_MYSQL, "DB") != 0) {
 		fprintf(stderr, "DB module initialization failed\n\n");
 		return EXIT_FATAL;
 	}
 
-	if (glite_lbu_DBConnect(db, dbcs) != 0) goto dberr;
-	if (glite_lbu_PrepareStmt(db, "INSERT INTO mountpoints (pointvolume, pointdir, volume) VALUES (?, ?, ?)", &points_stmt) != 0) goto dberr;
-	if (glite_lbu_PrepareStmt(db, "INSERT INTO rights (volume, dir, login, rights) VALUES (?, ?, ?, ?)", &rights_stmt) != 0) goto dberr;
+	if (glite_lbu_DBConnect(ctx.db, dbcs) != 0) goto dberr;
+	if (glite_lbu_PrepareStmt(ctx.db, "INSERT INTO mountpoints (pointvolume, pointdir, volume) VALUES (?, ?, ?)", &ctx.points_stmt) != 0) goto dberr;
+	if (glite_lbu_PrepareStmt(ctx.db, "INSERT INTO rights (volume, dir, login, rights) VALUES (?, ?, ?, ?)", &ctx.rights_stmt) != 0) goto dberr;
 
-	if (browse(basedir, action, NULL) == BROWSE_ACTION_ABORT) retval = EXIT_FATAL;
-	if (err) retval = EXIT_ERRORS;
+	printf("%s: ", ctx.volume); fflush(stdout);
+	gettimeofday(&ctx.begin, NULL);
+	glite_lbu_Transaction(ctx.db);
+	if (browse(ctx.basedir, action, &ctx) == BROWSE_ACTION_ABORT) retval = EXIT_FATAL;
+	printf("%zd ", ctx.count); fflush(stdout);
+	glite_lbu_Commit(ctx.db);
+	gettimeofday(&ctx.end, NULL);
+	printf("done, time %lf\n", timeval2double(&ctx.end) - timeval2double(&ctx.begin)); fflush(stdout);
+	if (ctx.was_err) retval = EXIT_ERRORS;
 
-	glite_lbu_FreeStmt(&points_stmt);
-	glite_lbu_FreeStmt(&rights_stmt);
-	glite_lbu_DBClose(db);
+	glite_lbu_FreeStmt(&ctx.points_stmt);
+	glite_lbu_FreeStmt(&ctx.rights_stmt);
+	glite_lbu_DBClose(ctx.db);
 
 err:
 	free(dbcs);
-	glite_lbu_FreeDBContext(db);
+	glite_lbu_FreeDBContext(ctx.db);
 	glite_common_log_fini();
 
 	return retval;
 dberr:
-	print_DBError(db);
+	print_DBError(ctx.db);
 	goto err;
 }
